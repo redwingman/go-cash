c050f20ca9bdf97dd8836258359fa8dcc99fff91
xiangwei <xiangwei22@protonmail.com>
2022-04-17 17:57:54 +0200

    pandora cash settings

--- config/config_stake/stake.go
+++ config/config_stake/stake.go
@@ -10,7 +10,7 @@ func GetRequiredStake(blockHeight uint64) (requiredStake uint64) {
 	var err error
 
 	var amount uint64
-	if blockHeight < 30000 {
+	if blockHeight < 30000 { //~5 weeks
 		amount = 200
 	} else {
 		amount = 5000
@@ -35,9 +35,9 @@ func GetPendingStakeWindow(blockHeight uint64) uint64 {
 
 	if blockHeight < 10000 { //11 days
 		return 10 //0.27h
-	} else if blockHeight < 50000 {
+	} else if blockHeight < 50000 { //8 weeks
 		return 120 //3.3 h
 	}
 
-	return 1000 //1.1 days
+	return 4000 //4.67 days
 }
