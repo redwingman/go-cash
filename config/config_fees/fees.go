082cbd12485bb0129e57e57c24043175bd30057b
xiangwei22 <xiangwei22@protonmail.com>
2022-10-06 06:56:24 -0500

    new fees

--- config/config_fees/fees.go
+++ config/config_fees/fees.go
@@ -1,9 +1,9 @@
 package config_fees
 
 var (
-	FEE_PER_BYTE             = uint64(50)
-	FEE_PER_BYTE_ZETHER      = uint64(130)
-	FEE_PER_BYTE_EXTRA_SPACE = uint64(2200)
+	FEE_PER_BYTE             = uint64(15)
+	FEE_PER_BYTE_ZETHER      = uint64(30)
+	FEE_PER_BYTE_EXTRA_SPACE = uint64(2000)
 )
 
 func ComputeTxFee(size, feePerByte, extraSpace, feePerByeExtraSpace uint64) uint64 {
